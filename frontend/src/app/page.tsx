ca7c2f10dce7e3a2750478c97bd386f10a63a5f2
Alexy Viegas <alexyviegas@gmail.com>
2024-03-12 12:27:02 -0300

    Add react-hook-form and zod dependencies

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,6 @@
+import EnterYourName from '@/components/enter-your-name';
 import VoteCTA from '@/components/vote-cta';
+import VoteScreen from '@/components/vote-screen';
 import WelcomeScreen from '@/components/welcome-screen';
 
 export default function Home() {
@@ -7,6 +9,8 @@ export default function Home() {
       <div className='z-10 max-w-5xl flex-col w-full items-center justify-between font-mono text-sm lg:flex'>
         <WelcomeScreen />
         <VoteCTA />
+        <EnterYourName />
+        <VoteScreen />
       </div>
     </main>
   );
--- frontend/src/components/vote-cta.tsx
+++ frontend/src/components/vote-cta.tsx
@@ -4,8 +4,8 @@ export default function VoteCTA() {
   return (
     <div>
       <h2>
-        It only takes a few seconds. Just enter your name and you'll be ready to
-        go!
+        It only takes a few seconds. Just enter your name and you&apos;ll be
+        ready to go!
       </h2>
       <Button>Vote Now</Button>
     </div>
