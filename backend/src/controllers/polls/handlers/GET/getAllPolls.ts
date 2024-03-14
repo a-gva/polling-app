59c5c951b9efded6ab5e9889e71d15c3686d5baf
Alexy Viegas <alexyviegas@gmail.com>
2024-03-13 22:37:27 -0300

    Add node-cache package and implement caching for getAllPolls and getPollById

--- backend/src/controllers/polls/handlers/GET/getAllPolls.ts
+++ backend/src/controllers/polls/handlers/GET/getAllPolls.ts
@@ -1,15 +1,28 @@
 import { PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express';
+import NodeCache from 'node-cache';
 
 const prisma = new PrismaClient();
+const myCache = new NodeCache();
 
 export async function getAllPolls(req: Request, res: Response) {
   try {
-    const allPolls = await prisma.poll.findMany({
-      orderBy: {
-        id: 'asc',
-      },
-    });
+    let allPolls = myCache.get('allPolls');
+
+    if (!allPolls) {
+      console.log('Fetching all polls from DB...');
+      allPolls = await new Promise((resolve) =>
+        setTimeout(async () => {
+          const polls = await prisma.poll.findMany({
+            orderBy: {
+              id: 'asc',
+            },
+          });
+          myCache.set('allPolls', polls, process.env.CACHE_TIMEOUT);
+          resolve(polls);
+        }, 1000)
+      );
+    }
     res.status(200).send(allPolls);
   } catch (err) {
     console.error(`🔴 ERROR: ${err.message}`);
--- backend/src/controllers/polls/handlers/GET/getPollById.ts
+++ backend/src/controllers/polls/handlers/GET/getPollById.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express';
+import NodeCache from 'node-cache';
 import { z } from 'zod';
 import { dbPollId } from '../../schema';
 
 const prisma = new PrismaClient();
+const myCache = new NodeCache();
 
 export async function getPollById(req: Request, res: Response) {
   try {
@@ -19,11 +21,38 @@ export async function getPollById(req: Request, res: Response) {
       return;
     }
 
-    const poll = await prisma.poll.findUnique({
-      where: {
-        id: parsedId,
-      },
-    });
+    let poll = myCache.get(`poll_${parsedId}`);
+
+    if (!poll) {
+      console.log(`🔴 Poll "${parsedId}" not found in cache...`);
+      console.log(`🔴 Fetching poll "${parsedId}" from DB...`);
+
+      poll = await new Promise((resolve) =>
+        setTimeout(async () => {
+          const pollFromDB = await prisma.poll.findUnique({
+            where: {
+              id: parsedId,
+            },
+          });
+          resolve(pollFromDB);
+        }, 1000)
+      );
+
+      if (poll) {
+        myCache.set(
+          `poll_${parsedId}`,
+          poll,
+          Number(process.env.CACHE_TIMEOUT)
+        );
+        console.log(
+          `🟢 Poll "${parsedId}" fetched from DB and stored in cache!`
+        );
+      } else {
+        console.log(`🔴 Poll "${parsedId}" not found in DB...`);
+      }
+    } else {
+      console.log(`🟢 Poll "${parsedId}" found in cache!`);
+    }
     res.status(200).send(poll);
   } catch (err) {
     res.send(err.message);
