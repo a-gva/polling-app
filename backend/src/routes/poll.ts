afb9a7b9f8e96eced4e6f62f45eb3b00ab3f9f44
Alexy Viegas <alexyviegas@gmail.com>
2024-03-13 21:42:45 -0300

    Refactor poll routes

--- backend/src/routes/poll.ts
+++ backend/src/routes/poll.ts
@@ -6,13 +6,8 @@ export const router = express.Router();
 const { createPoll, deletePoll, getAllPolls, getPollById, updatePoll } =
   pollControllers;
 
-// CREATE
 router.post('/', createPoll);
-// DELETE
 router.delete('/:id', deletePoll);
-// READ
 router.get('/:id', getPollById);
-// READ ALL
 router.get('/', getAllPolls);
-// UPDATE
 router.patch('/:id', updatePoll);
