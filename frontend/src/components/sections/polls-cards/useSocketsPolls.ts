227b1d520f27382bd5610439c5272e05151c499a
Alexy Viegas <alexyviegas@gmail.com>
2024-03-21 19:51:50 -0300

    Refactor useSocketsPolls to use useCallback and safeParse

--- frontend/src/components/sections/polls-cards/useSocketsPolls.ts
+++ frontend/src/components/sections/polls-cards/useSocketsPolls.ts
@@ -1,7 +1,7 @@
 import { pollsSchema, pollsWithResultsSchema } from '@/schema';
 import { useSocket } from '@/socket/provider';
-import { SinglePollProps, VotesRegistry } from '@/types';
-import { useEffect, useState } from 'react';
+import { PollsProps, SinglePollProps, VotesRegistry } from '@/types';
+import { useCallback, useEffect, useState } from 'react';
 
 export default function useSocketsPolls() {
   const socket = useSocket();
@@ -11,99 +11,67 @@ export default function useSocketsPolls() {
   const [allPollsVotes, setAllPollsVotes] = useState<VotesRegistry | null>({});
   const [lastCreatedPoll, setLastCreatedPoll] = useState('');
 
+  const onConnect = useCallback(() => setIsConnected(true), []);
+  const onDisconnect = useCallback(() => setIsConnected(false), []);
+  const onNewPollCreated = useCallback(
+    (newPollId: string) => setLastCreatedPoll(newPollId),
+    []
+  );
+  const onAllPollsDeleted = useCallback(() => {
+    setLastCreatedPoll('');
+    setAllPolls(null);
+  }, []);
+
   useEffect(() => {
-    const onConnect = () => {
-      setIsConnected(true);
-    };
-
-    function onDisconnect() {
-      setIsConnected(false);
-    }
-
-    function onNewPollCreated(newPollId: string) {
-      setLastCreatedPoll(newPollId);
-    }
-
-    function onAllPollsDeleted(message: string) {
-      setLastCreatedPoll('');
-      setAllPolls(null);
-    }
-
     if (socket) {
       socket.on('connect', onConnect);
       socket.on('disconnect', onDisconnect);
       socket.on('newPollCreated', onNewPollCreated);
       socket.on('allPollsDeleted', onAllPollsDeleted);
-    }
 
-    return () => {
-      if (socket) {
+      socket.on('allPolls', (allPolls: PollsProps[]) => {
+        const parsedAllPolls = pollsSchema.safeParse(allPolls);
+        if (parsedAllPolls.success) {
+          setAllPolls(parsedAllPolls.data);
+        } else {
+          console.error('Error parsing allPolls:', parsedAllPolls.error);
+        }
+      });
+
+      socket.on('allPollsVotes', (allPollsVotes: VotesRegistry) => {
+        const parsedAllPollsVotes =
+          pollsWithResultsSchema.safeParse(allPollsVotes);
+        if (parsedAllPollsVotes.success) {
+          setAllPollsVotes(parsedAllPollsVotes.data);
+        } else {
+          console.error(
+            'Error parsing allPollsVotes:',
+            parsedAllPollsVotes.error
+          );
+        }
+      });
+
+      if (isConnected) {
+        socket.emit('readyForData');
+      }
+
+      return () => {
         socket.off('connect', onConnect);
         socket.off('disconnect', onDisconnect);
         socket.off('newPollCreated', onNewPollCreated);
-      }
-    };
-  }, [socket, lastCreatedPoll]);
-
-  useEffect(() => {
-    if (socket) {
-      socket.on('allPolls', (dataBuffer) => {
-        try {
-          // Convert the ArrayBuffer to a Buffer
-          const data = Buffer.from(dataBuffer);
-
-          // Convert the Buffer to a string
-          const dataString = data.toString('utf8');
-
-          // Parse the string to a JavaScript object
-          const parsedAllPolls = pollsSchema.parse(JSON.parse(dataString));
-
-          setAllPolls(parsedAllPolls);
-        } catch (error) {
-          console.error('Error parsing allPolls:', error);
-          return;
-        }
-      });
-
-      socket.on('allPollsVotes', (dataBuffer) => {
-        try {
-          console.log('dataBuffer:', dataBuffer);
-
-          // Convert the ArrayBuffer to a Buffer
-          const data = Buffer.from(dataBuffer);
-
-          // Convert the Buffer to a string
-          const dataString = data.toString('utf8');
-
-          // Parse the string to a JavaScript object
-          const parsedAllPollsVotes = pollsWithResultsSchema.parse(
-            JSON.parse(dataString)
-          );
-
-          setAllPollsVotes(parsedAllPollsVotes);
-        } catch (error) {
-          console.log('pollsVotes:', dataBuffer);
-          console.log('Error parsing allPollsVotes:', error);
-          return;
-        }
-      });
-
-      return () => {
+        socket.off('allPollsDeleted', onAllPollsDeleted);
         socket.off('allPolls');
         socket.off('allPollsVotes');
       };
     }
-  }, []);
-
-  useEffect(() => {
-    if (isConnected) {
-      try {
-        socket?.emit('readyForData');
-      } catch (error) {
-        console.error('Error emitting readyForData:', error);
-      }
-    }
-  }, [isConnected, socket]);
+  }, [
+    socket,
+    isConnected,
+    onConnect,
+    onDisconnect,
+    onNewPollCreated,
+    onAllPollsDeleted,
+  ]);
 
   return { isConnected, allPolls, allPollsVotes, setAllPollsVotes };
 }
