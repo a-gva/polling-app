77b0519b43dbbb8889a2c6b22a3ef2cbe5497dc6
Alexy Viegas <alexyviegas@gmail.com>
2024-03-25 16:24:19 -0300

    Add turborepo

--- frontend/src/data/mockVotes.test.ts
+++ frontend/src/data/mockVotes.test.ts
@@ -1,9 +1,10 @@
 import { beatlesMembers, generateMockVotes } from '@/data/mockVotes';
+import { expect } from '@jest/globals';
 
 describe('mockVotes', () => {
   it('should return an array of votes', () => {
-    const result = generateMockVotes(10, beatlesMembers);
-    expect(result).toHaveLength(10);
+    const result1 = generateMockVotes(10, beatlesMembers);
+    expect(result1).toHaveLength(10);
     const result2 = generateMockVotes(50, beatlesMembers);
     expect(result2).toHaveLength(50);
   });
