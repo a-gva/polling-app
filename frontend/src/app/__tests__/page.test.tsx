eea1f7d759a851478c3c7617635fb3769b670606
Alexy Viegas <alexyviegas@gmail.com>
2024-03-25 19:20:08 -0300

    Remove Home component test

--- frontend/src/app/__tests__/page.test.tsx
+++
@@ -1,9 +0,0 @@
-import Home from '@/app/page';
-import renderer from 'react-test-renderer';
-
-describe('Home', () => {
-  it('renders correctly', () => {
-    const tree = renderer.create(<Home />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-});
